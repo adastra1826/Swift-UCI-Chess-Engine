5391af2743c8d0d34f00caea9dba47291c649b72
Nicholas Doherty <nicholasdoherty95@gmail.com>
2024-01-26 19:42:42 -0500

    Added _Options to _Engine settings

--- Swift/Settings.swift
+++ Swift/Settings.swift
@@ -19,16 +19,19 @@ class Settings {
     
     class _Engine {
         
+        let options: _Options
+        
         private let queue: DispatchQueue
         private var maxNumberSearchThreads: Int
         
         init() {
+            options = _Options()
             queue = DispatchQueue(label: "com.peerlessApps.chess.settings_EngineQueue")
             maxNumberSearchThreads = DefaultEngineSettings.maxNumberSearchThreads
         }
         
         struct DefaultEngineSettings {
-            static let maxNumberSearchThreads = 10
+            static let maxNumberSearchThreads = 3
         }
         
         func getMaxThreads() -> Int {
