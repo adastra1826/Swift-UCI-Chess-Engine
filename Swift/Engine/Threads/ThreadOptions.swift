916eea67e2694a09af5ce36e7da7f7ceff967242
Nicholas Doherty <nicholasdoherty95@gmail.com>
2024-04-05 01:06:24 -0400

    Project change

---
+++ Swift/Engine/Threads/ThreadOptions.swift
@@ -0,0 +1,14 @@
+//
+//  ThreadOptions.swift
+//  Chess-Engine-UCI
+//
+//  Created by Nicholas Doherty on 4/4/24.
+//
+
+import Foundation
+
+struct ThreadOptions {
+    
+    let threadUUID: UUID
+    
+}
