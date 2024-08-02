aab79f14035394154a96cb464f4d434d4d2c5fda
Nicholas Doherty <nicholasdoherty92@gmail.com>
2024-08-02 17:08:59 -0400

    Move from threads to dispatch group, remove the quitswitch bool flag, rely on semaphore siignal to quit program

--- Swift/EngineMaster.swift
+++ Swift/EngineMaster.swift
@@ -1,5 +1,5 @@
 //
-//  MasterSync.swift
+//  EngineMaster.swift
 //  Chess-Engine-UCI
 //
 //  Created by Nicholas Doherty on 1/10/24.
@@ -9,74 +9,74 @@ import Foundation
 
 class EngineMaster {
     
+    // Dependency injections
+    private let settings: Parameters
+    private let sharedData: SharedData
+    
+    // Privately initialized
     private let inputWrapper: SwiftInputWrapper
     private let outputWrapper: SwiftOutputWrapper
     private let engine: Engine
     
     private let masterThreadGroup: DispatchGroup
     
-    private var quitSwitch: Bool
-    
-    init() {
+    init(_ settings: Parameters, _ sharedData: SharedData) {
+        
+        self.settings = settings
+        self.sharedData = sharedData
         
         self.inputWrapper = SwiftInputWrapper()
         self.outputWrapper = SwiftOutputWrapper()
-        self.engine = Engine(outputWrapper)
+        self.engine = Engine(settings.engine, sharedData, outputWrapper)
         
         masterThreadGroup = DispatchGroup()
-        
-        quitSwitch = sharedData.masterQuit()
     }
     
-    public func bstartAll() {
+    public func start() {
         
-        let inputThread = Thread { [weak self] in
-            self?.startInput()
+        // Initialize default settings
+        initDefaultSettings()
+        
+        // Initialize primary threads
+        let inputQueue = DispatchQueue(label: "com.PeerlessApps.EngineMaster.input")
+        let outputQueue = DispatchQueue(label: "com.PeerlessApps.EngineMaster.output")
+        let engineQueue = DispatchQueue(label: "com.PeerlessApps.EngineMaster.engine")
+        
+        // Start primary threads
+        inputQueue.async(group: masterThreadGroup) { [weak self] in
+            self?.inputWrapper.start()
         }
         
-        let outputThread = Thread { [weak self] in
-            self?.startOutput()
+        outputQueue.async(group: masterThreadGroup) { [weak self] in
+            self?.outputWrapper.start()
         }
         
-        let engineThread = Thread { [weak self] in
-            self?.startEngine()
+        engineQueue.async(group: masterThreadGroup) { [weak self] in
+            self?.engine.start()
         }
         
-        inputThread.start()
-        outputThread.start()
-        engineThread.start()
+        // Wait for the signal to quit the application
+        waitForQuit()
         
-        // Artificial wait
+        // Clean up resources
+        breakdown()
+    }
+    
+    private func initDefaultSettings() {
+        
+    }
+    
+    // Wait for the master quit switch to be set to true before terminating the main thread, which immediately terminates the program
+    private func waitForQuit() {
         while true {
-            
-            Thread.sleep(forTimeInterval: 0.5)
-            //log.verbose(".")
-            
-            if sharedData.masterQuit() {
-                log.info("Break from MasterSynchronizer")
+            let result = sharedData.quitSwitchSemaphore.wait(timeout: .now() + 1)
+            if result == .success {
+                log.info("Master quit set to true, terminating main thread")
                 break
+            } else {
+                log.verbose("Timed out")
             }
         }
-        
-        
-        breakdown()
-        
-    }
-    
-    func stopAll() {
-        
-    }
-    
-    func startInput() {
-        inputWrapper.start()
-    }
-    
-    func startOutput() {
-        outputWrapper.start()
-    }
-    
-    func startEngine() {
-        engine.start()
     }
     
     func commandEngine(_ command: TopLevelCommand, _ arguments: [String]) {
